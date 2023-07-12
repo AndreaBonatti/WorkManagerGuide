4c80ea8d7d78e6fd4bc05aab38e5cd3d0ae19a95
Andrea Bonatti <andreabonatti92@gmail.com>
2023-07-12 17:04:06 +0200

    Initial commit

---
+++ app/src/main/java/com/andreabonatti92/workmanagerguide/ColorFilterWorker.kt
@@ -0,0 +1,56 @@
+package com.andreabonatti92.workmanagerguide
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.LightingColorFilter
+import android.graphics.Paint
+import androidx.core.net.toFile
+import androidx.core.net.toUri
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
+class ColorFilterWorker(
+    private val context: Context,
+    private val workerParams: WorkerParameters
+) : CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result {
+        val imageFile = workerParams.inputData.getString(WorkerKeys.IMAGE_URI)
+            ?.toUri()
+            ?.toFile()
+        delay(5000L)
+        return imageFile?.let { file ->
+            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+            val resultBitmap = bitmap.copy(bitmap.config, true)
+            val paint = Paint()
+            paint.colorFilter = LightingColorFilter(0x08FF04, 1)
+            val canvas = Canvas(resultBitmap)
+            canvas.drawBitmap(resultBitmap, 0f, 0f, paint)
+
+            withContext(Dispatchers.IO) {
+                val resultImageFile = File(context.cacheDir, "new-image.jpg")
+                val outputStream = FileOutputStream(resultImageFile)
+                val successful = resultBitmap.compress(
+                    Bitmap.CompressFormat.JPEG,
+                    90,
+                    outputStream
+                )
+                if (successful) {
+                    Result.success(
+                        workDataOf(
+                            WorkerKeys.FILTER_URI to resultImageFile.toUri().toString()
+                        )
+                    )
+                } else Result.failure()
+            }
+        } ?: Result.failure()
+    }
+}
